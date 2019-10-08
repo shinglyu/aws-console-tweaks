20ffc80b393506eabac3a8c88327caf40fa57fe9
Shing Lyu <Shing.Lyu@dazn.com>
2019-10-08 17:56:45 +0200

    feat: copy the s3 name directly to clipboard

--- s3_home.js
+++ s3_home.js
@@ -1,4 +1,31 @@
-setTimeout(function() {
+// credit: https://stackoverflow.com/questions/3436102/copy-to-clipboard-in-chrome-extension
+function copyTextToClipboard(text) {
+  //Create a textbox field where we can insert text to. 
+  var copyFrom = document.createElement("textarea");
+
+  //Set the text content to be the text you wished to copy.
+  copyFrom.textContent = text;
+
+  //Append the textbox field into the body as a child. 
+  //"execCommand()" only works when there exists selected text, and the text is inside 
+  //document.body (meaning the text is part of a valid rendered HTML element).
+  document.body.appendChild(copyFrom);
+
+  //Select all the text!
+  copyFrom.select();
+
+  //Execute command
+  document.execCommand('copy');
+
+  //(Optional) De-select the text using blur(). 
+  copyFrom.blur();
+
+  //Remove the textbox field from the document.body, so no other JavaScript nor 
+  //other elements can get access to this.
+  document.body.removeChild(copyFrom);
+}
+
+setTimeout(function() { // TODO: retry until the elements appear
     [].forEach.call(document.querySelectorAll("td.bucket"), function(title) {
         console.log(title)
         const copyButton = document.createElement('a')
@@ -6,7 +33,7 @@ setTimeout(function() {
         copyButton.style.fontSize = "24px";
         copyButton.onclick = function(){
             return function() {
-                alert(title.querySelector("span.list-view-item-name").innerText)
+                copyTextToClipboard(title.querySelector("span.list-view-item-name").innerText)
             }
         }()
         title.prepend(copyButton)
